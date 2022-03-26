80ccec928668ea6492e817401b8f227e8e7fffa0
kohei <bhkbb0551@gmail.com>
2022-03-27 00:38:19 +0900

    init project

---
+++ src/main/java/com/user/manegiment/basis/UserManegimentApplication.java
@@ -0,0 +1,13 @@
+package com.user.manegiment.basis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManegimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManegimentApplication.class, args);
+	}
+
+}
