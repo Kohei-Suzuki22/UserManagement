e6667526fbf3dfe24c4cd2cbbc488863dab1aca4
kohei <bhkbb0551@gmail.com>
2022-04-03 19:39:38 +0900

    create signup view

---
+++ src/main/java/com/user/manegiment/basis/controller/SignupController.java
@@ -0,0 +1,31 @@
+package com.user.manegiment.basis.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.user.manegiment.basis.service.UserService;
+
+@Controller
+@RequestMapping("/user")
+public class SignupController {
+	
+	@Autowired
+	private UserService userService;
+
+	@GetMapping("/signup")
+	public String getSignup(Model model){
+		
+		model.addAttribute("genderMap", userService.getGenderMap());
+		
+		return "user/signup";
+	}
+	
+	@PostMapping("/signup")
+	public String postSignup() {
+		return "redirect:/login";
+	}
+}	
