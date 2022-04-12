09ab9609498c6bad51d8318b13285041d6bd6302
kohei <bhkbb0551@gmail.com>
2022-04-13 08:45:03 +0900

    add update delete userOne

--- src/main/java/com/user/manegiment/basis/controller/UserDetailController.java
+++ src/main/java/com/user/manegiment/basis/controller/UserDetailController.java
@@ -4,35 +4,65 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.user.manegiment.basis.entity.MasterUser;
 import com.user.manegiment.basis.form.UserDetailForm;
+import com.user.manegiment.basis.form.validation.GroupOrder;
 import com.user.manegiment.basis.service.user.UserService;
 
 @Controller
 @RequestMapping("/user")
 public class UserDetailController {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private ModelMapper modelMapper;
 
+
 	@GetMapping("/detail/{userId:.+}")
 	public String getUserDetail(Model model, @PathVariable String userId, UserDetailForm form) {
-		
-		
-		MasterUser user = userService.getOne(userId);
+
+		MasterUser user = userService.getUserOne(userId);
 		user.setPassword(null);
-		
+
 		form = modelMapper.map(user, UserDetailForm.class);
-		
-		model.addAttribute("userDetailForm",form);
-		return "user/detail";
-		
+
+		model.addAttribute("userDetailForm", form);
+		return "/user/detail";
+
+	}
+
+	@PostMapping(value = "/detail", params = "update")
+	public String updateUser(Model model, @ModelAttribute @Validated(GroupOrder.class) UserDetailForm form,
+			BindingResult bindingResult) {
+
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("validationError", bindingResult.getAllErrors());
+			return getUserDetail(model, form.getUserId(), form);
+		}
+
+		MasterUser user = modelMapper.map(form, MasterUser.class);
+
+		userService.updateUserOne(user);
+
+		return getUserDetail(model, form.getUserId(), form);
+	}
+
+	@PostMapping(value = "/detail", params = "delete")
+	public String deleteUser(UserDetailForm form) {
+
+		userService.deleteUserOne(form.getUserId());
+
+		// serviceImpl
+		return "redirect:/user/list";
 	}
 }
--- src/main/java/com/user/manegiment/basis/form/UserDetailForm.java
+++ src/main/java/com/user/manegiment/basis/form/UserDetailForm.java
@@ -2,14 +2,30 @@ package com.user.manegiment.basis.form;
 
 import java.util.Date;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.user.manegiment.basis.form.validation.ValidGroup1;
+import com.user.manegiment.basis.form.validation.ValidGroup2;
+
 import lombok.Data;
 
 @Data
 public class UserDetailForm {
 	
-	
+	@NotBlank(groups = ValidGroup1.class)
+	@Email(groups = ValidGroup2.class)
 	private String userId;
+	
+	@NotBlank(groups = ValidGroup1.class)
+	@Length(min = 4, max = 100, groups = ValidGroup2.class)
+	@Pattern(regexp = "^[a-zA-Z0-9]+$", groups = ValidGroup2.class)
 	private String password;
+	
+	@NotBlank(groups = ValidGroup1.class)
 	private String userName;
 	private Date birthday;
 	private Integer age;
