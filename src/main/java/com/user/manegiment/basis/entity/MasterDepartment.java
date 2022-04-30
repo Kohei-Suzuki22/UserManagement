e7f3f20fa9e929cab89a8b4153ef975feb50fabc
kohei <bhkbb0551@gmail.com>
2022-04-30 20:20:42 +0900

    JPA manyToOne  join masterUser.departmentId = masterDepartment.departmentId

--- src/main/java/com/user/manegiment/basis/entity/MasterDepartment.java
+++ src/main/java/com/user/manegiment/basis/entity/MasterDepartment.java
@@ -1,10 +1,17 @@
 package com.user.manegiment.basis.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "master_department")
 public class MasterDepartment {
 
+	@Id
 	private Integer departmentId;
 	private String departmentName;
 }
--- src/main/java/com/user/manegiment/basis/entity/MasterUser.java
+++ src/main/java/com/user/manegiment/basis/entity/MasterUser.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -24,7 +26,8 @@ public class MasterUser {
 	private Integer gender;
 	private Integer departmentId;
 	private String role;
-	@Transient
+	@ManyToOne(optional = true)
+	@JoinColumn(insertable = false, updatable = false, name="departmentId", referencedColumnName = "departmentId")
 	private MasterDepartment department;
 	@Transient
 	private List<Salary> salaryList;
